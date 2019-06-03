0454eddc5cae192af0843cb3f893f2c716fbc583
Artem Sazonov <a.sazonau@gramophone.by>
2019-06-03 18:29:28 +0300

    some

--- test/TableViewController.swift
+++ test/TableViewController.swift
@@ -1,11 +1,21 @@
 import UIKit
 import Kingfisher
 
-final class ViewController: UITableViewController {
+final class TableViewController: UITableViewController {
+    
+    let refControl: UIRefreshControl = {
+        let refresh = UIRefreshControl(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        refresh.addTarget(self, action: #selector(TableViewController.refresh), for: UIControl.Event.valueChanged)
+        return refresh
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.refreshControl = refControl
+    }
+    
+    @objc private func refresh() {
+        refreshControl?.endRefreshing()
     }
     
 }
