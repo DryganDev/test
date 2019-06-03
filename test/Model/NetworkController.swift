443dde5c8d6b554e3e0e3b5a45a43a67bed0ecd3
Artem Sazonov <a.sazonau@gramophone.by>
2019-06-03 18:14:33 +0300

    init

---
+++ test/Model/NetworkController.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkController.swift
+//  test
+//
+//  Created by Drygan on 6/3/19.
+//  Copyright © 2019 drygan.dev. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+    static let shared = NetworkController()
+    private init() {}
+}
---
+++ test/TableViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Kingfisher
+
+final class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
